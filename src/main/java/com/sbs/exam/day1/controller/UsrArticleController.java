b0d3e7ded453afa10498733639b410b48b438f90
HyeonGi2 <tmskc56@gmail.com>
2022-07-13 13:15:20 +0900

    테스용 게시물 10개 자동생성

---
+++ src/main/java/com/sbs/exam/day1/controller/UsrArticleController.java
@@ -0,0 +1,61 @@
+package com.sbs.exam.day1.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.exam.day1.vo.Article;
+
+@Controller
+public class UsrArticleController {
+	private int articlesLastId;
+	private List<Article> articles;
+
+	public UsrArticleController() {
+		articlesLastId = 0;
+		articles = new ArrayList<>();
+
+		makeTestData();
+
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			Article article = new Article(id, title, body);
+			articles.add(article);
+			articlesLastId = id;
+		}
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		int id = articlesLastId + 1;
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		articlesLastId = id;
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articles;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		return id + "번 게시물을 삭제하였습니다.";
+	}
+
+}
