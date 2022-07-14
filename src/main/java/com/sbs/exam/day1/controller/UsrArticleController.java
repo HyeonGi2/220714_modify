aba07080c42d3b062d2bfebf065deb3072ddeb21
HyeonGi2 <tmskc56@gmail.com>
2022-07-14 10:26:50 +0900

    게시물 수정 액션

--- src/main/java/com/sbs/exam/day1/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/day1/controller/UsrArticleController.java
@@ -15,7 +15,7 @@ public class UsrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
 	// 인스턴스 변수 끝
-	
+
 	// 생성자
 	public UsrArticleController() {
 		articlesLastId = 0;
@@ -24,43 +24,52 @@ public class UsrArticleController {
 		makeTestData();
 
 	}
-	
+
 	// 서비스 메서드 시작
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
-			
+
 			String title = "제목 " + i;
 			String body = "내용 " + i;
-			
+
 			writeArticle(title, body);
 		}
 	}
-	
+
 	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
-		
+
 		articles.add(article);
 		articlesLastId = id;
-		
+
 		return article;
 	}
+
 	private Article getArticle(int id) {
-		for ( Article article : articles) {
-			if ( article.getId() == id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				return article;
 			}
 		}
 		return null;
 	}
+
 	private void deleteArticle(int id) {
 		Article article = getArticle(id);
-		
+
 		articles.remove(article);
 	}
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+	}
 	// 서비스 메서드 끝
-	
-	//액션메서드 시작
+
+	// 액션메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -79,14 +88,31 @@ public class UsrArticleController {
 	@ResponseBody
 	public String doDelete(int id) {
 		Article article = getArticle(id);
-		
-		if ( article == null) {
+
+		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
-		
+
 		deleteArticle(id);
 
 		return id + "번 게시물을 삭제하였습니다.";
 	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + "번 게시물을 수정하였습니다.";
+	}
+
+	
+	
 	// 액션메서드 끝
 }
