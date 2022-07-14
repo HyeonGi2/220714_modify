957026e3e0e37db707f1f2fb23a807f51fc499b4
HyeonGi2 <tmskc56@gmail.com>
2022-07-14 10:37:14 +0900

    getArticle 액션 구현

--- src/main/java/com/sbs/exam/day1/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/day1/controller/UsrArticleController.java
@@ -83,6 +83,16 @@ public class UsrArticleController {
 
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		if( article == null ) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		return article;
+	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
@@ -96,6 +106,20 @@ public class UsrArticleController {
 		deleteArticle(id);
 
 		return id + "번 게시물을 삭제하였습니다.";
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + "번 게시물을 수정하였습니다.";
 	}
 	
 	@RequestMapping("/usr/article/doModify")
